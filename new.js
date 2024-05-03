fe625e50b300e42fb0b8ecdf458f85baa06a05bd
midhunmanikandan <midhunmanikandan870@gmail.com>
2024-05-03 12:08:49 +0530

    first commit

---
+++ new.js
@@ -0,0 +1,10 @@
+function prefix(){
+    let arr=['cat','car','fear','center'];
+    {
+      let l=arr.length;
+      for(let i=0;i<=l;i++){
+                                                                              
+      }
+  
+      }
+  }
